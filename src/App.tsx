bf39ede0f3a6be550d1eb1bc1f4ea4589b778fc0
Dan Stronge <dnlstronge@gmail.com>
2023-06-03 12:24:00 +0100

    [x] - add css reset to index

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Home from './Components/Home/Home';
+import classes from "./App.module.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className={classes.container}>
       <title>Monsters DB </title>
       <Home />
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import classes from "./App.module.css"
 import App from './App';
+import "./index.css"
 
 
 const root = ReactDOM.createRoot(
