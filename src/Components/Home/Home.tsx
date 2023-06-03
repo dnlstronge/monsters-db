f74d41078f577f6220faafd871ddcf86c3b6620a
Dan Stronge <dnlstronge@gmail.com>
2023-06-03 13:19:16 +0100

    [x] - set up basic fetch and test

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -7,18 +7,27 @@ const Home = () => {
   clicking button should issue fetch request and store data in state....
 
 */ 
-
+const [testMonsterOK, setTestMonsterOK] = useState(false)
 const [testMonster, settestMonster] = useState({
-
+  id: "",
+  name: "",
+  description: "",
+  hp: ""
 })
   
 const fetchMonster = async() => {
+  setTestMonsterOK(false)
   const response = await fetch("https://monsterdb-30be5-default-rtdb.europe-west1.firebasedatabase.app/monsters/A/Agromole.json")
   const data = await response.json()
+  setTestMonsterOK(true)
   console.log(data)
   console.log(response)
-  
-
+  settestMonster({
+        id: data.id,
+        name: data.name,
+        description: data.desc,
+        hp: data.hp
+       })
 }
 
   return (
@@ -30,6 +39,11 @@ const fetchMonster = async() => {
         <section className={classes.testsection}>
           <button onClick={fetchMonster} className={classes.btn}>Get Monster... </button>
         </section>
+        {testMonsterOK && 
+        <section className={classes.testresponse}>
+          <h5>Name: {testMonster.name}</h5>
+          <h6>Description: {testMonster.description}</h6>
+        </section>}
     </div>
   )
 }
