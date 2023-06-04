176bc6d9687a1f9295881070b7ab6be67ebfc432
Dan Stronge <dnlstronge@gmail.com>
2023-06-04 17:37:58 +0100

    [x] - map data into buttons

--- src/Components/Pages/CreaturesAZ/CreaturesAZ.tsx
+++ src/Components/Pages/CreaturesAZ/CreaturesAZ.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useFetchCreaturesAZ } from '../../Hooks/useFetchCreaturesAZ'
+import classes from "./CreaturesAz.module.css"
 
 /** get results and filter ???  */
 
@@ -8,36 +9,37 @@ const lettersAZ = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "
 
 
 const CreaturesAZ = () => {
-  const [letterSearch, setLetterSearch] = useState("")
-  const [showResults, setShowResults] = useState("")
-  const [testData, setTestData] = useState(null)
+  const [letterSearch, setLetterSearch] = useState(null)
+  const [showResults, setShowResults] = useState([])
+
 
 
 
 
   useEffect(() => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const fetchData = async () => {
+    const fetchData = async (term: string) => {
+
       console.log(letterSearch)
-      const response = await fetch(`https://monsterdb-30be5-default-rtdb.europe-west1.firebasedatabase.app/monsters/${letterSearch}.json`)
+      const response = await fetch(`https://monsterdb-30be5-default-rtdb.europe-west1.firebasedatabase.app/monsters/${term}/.json`)
       const data = await response.json()
-      const manageData = Object.keys(data)
-      //console.log(data)
-      setShowResults(data)
-      console.log(showResults)
-      //setShowResults(Object.keys(data))
-      // console.log("fetch successfull")
-      // console.log(Object.keys(data.A))
+      const manageData: any = Object.keys(data)
+      setShowResults(manageData)
+     
 
     }
-    fetchData()
+    if (letterSearch !== null) {
+      fetchData(letterSearch)
+    } else {
+      return
+    }
+
 
   }, [letterSearch])
 
+  /* To test fetch */
   useEffect(() => {
-    const data: any = Object.keys(showResults)
-   setTestData(data)
-   console.log(testData)
+    console.log(showResults)
   }, [showResults])
 
 
@@ -47,22 +49,23 @@ const CreaturesAZ = () => {
 
   return (
     <>
-      <div>Creatures AZ</div>
-      <div>
+      <div className={classes.head}>
+        <h5 className={classes.heading}>Creatures A - Z</h5>
+      </div>
+      <div className={classes.letters}>
         {lettersAZ.map((letter) => {
 
           return (
-
-            <div onClick={() => { handleClick(letter) }} key={letter}>{letter}</div>
+            <button onClick={() => { handleClick(letter) }} key={letter}>{letter}</button>
           )
         })}
       </div>
+      <div className={classes.results}>
+        {showResults.length > 0 && showResults.map((result) => {
+          return <button key={result}>{result}</button>
+        })}
+      </div>
 
-      {/* <div>{showResults.map((result) => {
-      return (
-        <div></div>
-      )
-    })}</div> */}
     </>
   )
 }
