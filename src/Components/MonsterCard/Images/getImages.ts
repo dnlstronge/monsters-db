606b7cf6bdc412760a726b27736a8d410d1f83ef
Dan Stronge <dnlstronge@gmail.com>
2023-06-05 08:46:01 +0100

    [x] - resolved partial issue with url

--- src/Components/MonsterCard/Images/getImages.ts
+++ src/Components/MonsterCard/Images/getImages.ts
@@ -1,9 +1,15 @@
 import useImage from "../../Hooks/useImage"
 
-export const getImages = async() => {
+
+/* Config :
+
+gs://monsterdb-30be5.appspot.com[xxxxxxxx/xxxxx]
+
+*/
+export const getImages = async(url: string) => {
     const images = []
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const getImage = await useImage(`gs://monsterdb-30be5.appspot.com/monsters/MonsterTest.png`)
+    const getImage = await useImage(url)
     images.push(getImage)
     console.log(getImage)
     return getImage;
--- src/Components/Pages/CreaturesAZ/CreatureCard.tsx
+++ src/Components/Pages/CreaturesAZ/CreatureCard.tsx
@@ -4,10 +4,13 @@ import classes from "./CreatureCard.module.css"
 import { getImages } from '../../MonsterCard/Images/getImages'
 
 const CreatureCard: React.FC<creatureCardProps> = (props) => {
+
+    // gs://monsterdb-30be5.appspot.com/monsters/agromole.png
     
     const [imageURLstate, setImageURLstate] = useState("")
     const findImage = async() => {
-        let imageURL = await getImages()
+        let imageProp = props.imageURL
+        let imageURL = await getImages(`gs://monsterdb-30be5.appspot.com/monsters/${props.imageURL}.png`)
         setImageURLstate(imageURL)
     }
     findImage()
