7f2dc516582f5274db120c9f0349a68ffeac395f
Dan Stronge <dnlstronge@gmail.com>
2023-06-06 08:39:20 +0100

    [x] - add loading logic for images

--- src/Components/Pages/CreaturesAZ/CreatureCard.tsx
+++ src/Components/Pages/CreaturesAZ/CreatureCard.tsx
@@ -8,26 +8,27 @@ const CreatureCard: React.FC<creatureCardProps> = (props) => {
     // gs://monsterdb-30be5.appspot.com/monsters/agromole.png
     
     const [imageURLstate, setImageURLstate] = useState("")
+    const [loadImage, setLoadImage] = useState(false)
     const findImage = async() => {
-        
+        setLoadImage(true)
         try {
-            // let imageProp = props.imageURL
-            // let name = "agromole"
-            // //console.log(imageProp)
             let imageURL = await getImages(`gs://monsterdb-30be5.appspot.com/monsters/${props.imageURL}.png`)
             setImageURLstate(imageURL)
+            setLoadImage(false)
         } catch (error) {
+            setLoadImage(false)
             return //console.log(error)
         }
        
     }
     findImage()
     
-
+        /* condtional style */
+        const imageLoad = 
   return (
     <div className={classes.container}>
         <h5>{props.name}</h5>
-        <img src={imageURLstate} alt=" monster"/>
+        <img className={classes.image}src={imageURLstate} alt=" monster"/>
     </div>
   )
 }
