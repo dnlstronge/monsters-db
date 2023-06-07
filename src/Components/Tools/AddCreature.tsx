405ef07c2a5955db5f2b9831681d4bbaafa3fff7
Dan Stronge <dnlstronge@gmail.com>
2023-06-07 12:54:06 +0100

    [x] - add state for POST

--- src/Components/Tools/AddCreature.tsx
+++ src/Components/Tools/AddCreature.tsx
@@ -1,11 +1,26 @@
 
-import React from "react"
+import React, { useState } from "react"
 import classes from "./AddCreature.module.css"
 
 const AddCreature = () => {
+
+    const [postCreatureState, setPostCreatureState] = useState(
+        {
+        magic: "",
+        attack: "",
+        defence: "",
+        desc: "",
+        hp: "",
+        id: "",
+        imageURL: "",
+        name: ""
+        }
+    )
     return (
         <>
-        <form></form>
+        <form>
+            <label htmlFor="name"></label>
+        </form>
             </>
     )
      
