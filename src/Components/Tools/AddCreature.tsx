90f6eeb139a038d298b3176e49dce6de51c0772c
Dan Stronge <dnlstronge@gmail.com>
2023-06-07 14:13:27 +0100

    [x] - add validation logic

--- src/Components/Tools/AddCreature.tsx
+++ src/Components/Tools/AddCreature.tsx
@@ -1,6 +1,7 @@
 
 import React, { useState } from "react"
 import classes from "./AddCreature.module.css"
+import { FormEventHandler } from "react"
 
 const AddCreature = () => {
 
@@ -18,20 +19,47 @@ const AddCreature = () => {
         name: ""
         }
     )
+    /* Validation */
+
+    const [formIsValid, setFormIsValid] = useState(false)
+    const [nameIsValid, setNameIsValid] = useState(false)
 
     /* Submit handler */
 
     const handleSubmit = () => {}
 
+    /* state handlers */
+
+    const handleName = (e: React.FormEvent<HTMLInputElement> ) => {
+        
+        const regex = /^[A-Za-z]+$/
+        const nameToValidate = e.currentTarget.value.trim()
+        setPostCreatureState({...postCreatureState, name:nameToValidate })
+        if(nameToValidate.length <= 16 && nameToValidate.length > 0
+            && nameToValidate.match(regex) 
+              ) {
+                setNameIsValid(true)
+                
+              } else {
+                setNameIsValid(false)
+              }
+    }
+
     return (
         <>
         <form onSubmit={handleSubmit}>
-            <label htmlFor="name"></label>
+             {/* Name */}
+            <section>
+            <label className={classes.label} htmlFor="name"></label>
+            <input className={classes.input} type="text" onChange={handleName}></input>
+            <p className={classes.nameInvalid}>Error - name should 16 chars or less, and contain only letters</p>
+            </section>
+            {/* description */}
             <label htmlFor="desc"></label>
             <label htmlFor="hp"></label>
             <label htmlFor="attack"></label>
             <label htmlFor="defence"></label>
-            
+            <button type="submit">Submit</button>
         </form>
             </>
     )
