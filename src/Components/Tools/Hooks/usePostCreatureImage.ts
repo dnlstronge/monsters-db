9dd14ea36bdf68fbb09c4c76eac323293f8f7dc6
Dan Stronge <dnlstronge@gmail.com>
2023-06-08 17:26:10 +0100

    [x] - add upload logic

--- src/Components/Tools/Hooks/usePostCreatureImage.ts
+++ src/Components/Tools/Hooks/usePostCreatureImage.ts
@@ -1,10 +1,13 @@
 import { ref } from "firebase/storage";
 import { projectStorage } from "../../../firebase/config";
 import { fileUpload } from "../../../Models/interface";
-
+import { uploadBytes } from "firebase/storage";
 
 const usePostCreatureImage: any = (file: fileUpload, name: string) => {
-    const fileData = file.data
-
+    const fileData = file.data!
+    const storageRef = ref(projectStorage, `/monsters/${name}`)
+    uploadBytes(storageRef, fileData).then((snapshot) => {
+        console.log('Uploaded a blob or file!');
+      });
 }
 export default usePostCreatureImage;
