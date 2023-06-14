c034287df1497562afb5f2418f356e3c5dc23116
Dan Stronge <dnlstronge@gmail.com>
2023-06-14 11:40:11 +0100

    [x] - fix get logic to self reset

--- src/Components/Pages/SearchResults/SearchResults.tsx
+++ src/Components/Pages/SearchResults/SearchResults.tsx
@@ -8,8 +8,29 @@ const SearchResults = () => {
 
   const [searchValid, setSearchIsValid] = useState(false)
   const [searchTerm, setSearchTerm] = useState("")
+  const [dataSet, setDataSet] = useState<any>()
+  
+  /*fetch helper */
   
 
+  useEffect(() => {
+    /* fetch  */
+    const fetchData = async() => {
+      const char = searchTerm.charAt(0).toUpperCase()
+      const response = await fetch(`https://monsterdb-30be5-default-rtdb.europe-west1.firebasedatabase.app/monsters/${char}/.json`)
+      const data = await response.json()
+      setDataSet(data)
+      
+    }
+
+    if(searchTerm.length > 0 && searchValid) {
+      fetchData()
+    } else {
+      console.log("Add error handlinng etc")
+    }
+    setSearchIsValid(false)
+    console.log(dataSet)
+  },  [dataSet, searchTerm, searchValid])
   return (
    
    <>
