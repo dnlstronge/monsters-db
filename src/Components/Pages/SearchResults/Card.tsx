e1d2f5295fc2c8df58142e7dcec85b2653c7e889
Dan Stronge <dnlstronge@gmail.com>
2023-06-16 13:41:16 +0100

    [x]

--- src/Components/Pages/SearchResults/Card.tsx
+++ src/Components/Pages/SearchResults/Card.tsx
@@ -4,11 +4,13 @@ import classes from "./Card.module.css"
 import { cardProps } from '../../../Models/types'
 
 const Card: React.FC<cardProps> = (props) => {
-  console.log(`here asshat: ${props.name}`)
+ 
   return (
-    <div>
-      <h4>A</h4>
-      <p>B</p>
+    <div className={classes.container}>
+      <h4 className={classes.heading}>{props.name}</h4>
+      <img className={classes.image}src={props.image} alt={props.name}/>
+      <p>{props.desc}</p>
+
     </div>
   )
 }
