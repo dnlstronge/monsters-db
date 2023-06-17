7978f6e08a90ab1c4970fe6290469c83dbe84e73
Dan Stronge <dnlstronge@gmail.com>
2023-06-17 11:07:44 +0100

    [x] - logic for login

--- src/Components/LoginSignup/Login.tsx
+++ src/Components/LoginSignup/Login.tsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import classes from "./Login.module.css"
+import { authLogin } from '../Auth/auth'
+
 
 const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    /* validators */
+    const emailValid = email.length > 0 && email.includes("@")
+    const regex = /^[0-9]/
+    const passwordValid = password.length >= 6 && password.includes(`${regex}`)
+    const loginValid = passwordValid && emailValid ? true : false
+    /* handlers */
+    const handleSubmit = () => {
+        if(loginValid) {
+            authLogin(email, password)
+        }
+    }
+
+    const handleEmail = (e: ChangeEvent<HTMLInputElement>) => {
+        // add validation
+        setEmail(e.currentTarget.value)
+    }
+    const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
+        // add validation
+        setPassword(e.currentTarget.value)
+    }
   return (
     <div className={classes.container}>
-        <label className={classes.label} htmlFor='username'>Username: </label>
-        <input id="username" className={classes.input} type="text"/>
+        <label className={classes.label} htmlFor='username'>Email:  </label>
+        <input onChange={handleEmail}id="email" className={classes.input} type="text"/>
         <label className={classes.label} htmlFor="password">Password: </label>
-        <input id="password" className={classes.input} type="password" ></input>
+        <input onChange={handlePassword } id="password" className={classes.input} type="password" ></input>
+        <button onClick={handleSubmit}>Login</button>
     </div>
   )
 }
