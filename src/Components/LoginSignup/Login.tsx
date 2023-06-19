81c832842a4d240f18600ad2f0daf040d0fd5354
Dan Stronge <dnlstronge@gmail.com>
2023-06-19 07:21:10 +0100

    [x] add fetch key + error handling

--- src/Components/LoginSignup/Login.tsx
+++ src/Components/LoginSignup/Login.tsx
@@ -14,6 +14,31 @@ type userDataState = {
     message: string | unknown
     data: null | any
 }
+const checkAdmin = async() => {
+    try {
+       const response = await fetch(`https://monsterdb-30be5-default-rtdb.europe-west1.firebasedatabase.app/adminKey/.json`)
+       if (response!.ok ){
+        const key = response.json()
+        return {
+            status: response.status,
+            error: false,
+            message: key
+        }
+       } else {
+        return {
+            status: response.status,
+            error: true,
+            message: `${response.status} - Key not found`
+        }
+       }
+    } catch (error) {
+       return {
+        status: "unable to fetch key",
+        error: true,
+        message: error
+       }
+    }
+}
 
 const Login: React.FC = () => {
     const [email, setEmail] = useState("")
